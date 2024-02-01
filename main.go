27721d886dcab05f2322a5b83fed61bb69b87eee
ap <evilgamer163@yandex.ru>
2024-02-01 11:01:19 +0400

    add db

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"database/sql"
 
+	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db := initDB("storage.db")
+	migrate(db)
+
+	e := echo.New()
+
+	e.GET("/tasks", func(c echo.Context) error { return c.JSON(200, "GET Tasks") })
+	e.PUT("/tasks", func(c echo.Context) error { return c.JSON(200, "PUT Tasks") })
+	e.DELETE("/tasks/:id", func(c echo.Context) error { return c.JSON(200, "DELETE Task "+c.Param("id")) })
+
+	e.Logger.Fatal(e.Start(":8000"))
+
+}
+
+func initDB(filepath string) *sql.DB {
+	db, err := sql.Open("sqlite3", filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	if db == nil {
+		panic("db nil")
+	}
+
+	return db
+}
+
+func migrate(db *sql.DB) {
+	sql := `CREATE TABLE IF NOT EXISTS tasks(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR NOT NULL
+    );`
+
+	_, err := db.Exec(sql)
+	if err != nil {
+		panic(err)
+	}
 }
