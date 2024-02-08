de5fc74518a517a18736346f34b8ebd1dabc87e3
ap <evilgamer163@yandex.ru>
2024-02-08 21:12:40 +0400

    add style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,33 +2,68 @@
 <div>
 	<h1>Tasks</h1>
 
-	<ul>
-		<li v-for="task in tasks" :key="task.ID">
-			{{task.Name}}
-		</li>
-	</ul>
+	<div class="content">
+
 	</div>
+
+	<AppAlert v-for="(err, idx) in errors" type="error" :message="err" :key="idx" />
+</div>
 </template>
 
 <script>
+import AppAlert from './components/AppAlert.vue'
+
 export default {
 	name: 'App',
 
+	components: { AppAlert },
+
 	data() {
 		return {
-			tasks: []
+			tasks: [],
+			errors: [],
 		}
 	},
 
 	mounted() {
-		this.$http.get('http://localhost:8000/tasks')
-		.then( response => {
-			this.tasks = response.data.Tasks
-		})
+		this.getTasks()
+	},
+
+	methods: {
+		async getTasks() {
+			this.$http.get('http://localhost:8000/tasks')
+			.then( response => {
+				this.tasks = response.data.Tasks
+			})
+			.catch( err => this.errors = [...this.errors, err])
+		}
 	}
 }
 </script>
 
-<style>
+<style lang="scss">
+* {
+	font-family: Arial, Helvetica, sans-serif;
+}
 
+body {
+	background: rgb(180, 207, 241);
+}
+
+h1 {
+	display: block;
+	background: rgb(77, 120, 238);
+	width: 95%;
+	margin: 0 auto;
+	text-align: center;
+	color: #ffffff;
+	border-radius: 5px;
+	padding: 10px;
+}
+
+.content {
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+}
 </style>
---
+++ frontend/src/components/AppAlert.vue
@@ -0,0 +1,41 @@
+<template>
+	<div
+		class="alert"
+		:class="{ 'error': type == 'error', 'warning': type == 'warning', 'success': type == 'success' }"
+	>
+		{{ message }}
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'AppAlert',
+
+	props: {
+		type: String,
+		message: String,
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+.alert {
+	width: 40%;
+	padding: 10px;
+	position: fixed;
+	top: 10px;
+	left: 50%;
+	transform: translateX(-50%);
+	border-radius: 5px;
+	z-index: 100;
+	color: #ffffff;
+	font-size: 18px;
+	font-weight: 600;
+	text-align: center;
+
+	&.error {
+		background: rgb(226, 52, 52);
+		border-top: 10px solid rgb(185, 63, 63);
+	}
+}
+</style>
